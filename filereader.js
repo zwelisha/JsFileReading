529c4fc3a1bbb6ad46c5f25119db0eac6427b672
godfreydlamini <zwescientist@gmail.com>
2020-06-12 20:57:38 +0200

    Finalised the function to read file

--- filereader.js
+++ filereader.js
@@ -1,6 +1,30 @@
 const fs = require('fs')
 
-fs.readFile('inspirationalquotes.txt', (error, data) => {
-    if (error) throw error;
-    console.log(data.toString());
-})
+/**
+ * Reads a file and return its content as an array of lines from the file
+ * @param  {String} filePath The path (url) to the file
+ * @return {Array}      The array with all lines from the file
+ */
+function readFileToArray(filePath){
+    try {
+        let data = fs.readFileSync(filePath, 'utf-8');
+        let lines = data.split('\n');
+        //No take all lines that are not empty and add them to quotes
+        let quotes = [];
+        for (let i = 0; i < lines.length; i++){
+            if (lines[i] != ''){
+                quotes.push(lines[i]);
+            }
+        }
+        
+        return quotes;
+    } catch (error) {
+        //You can replace this with a better message.
+        throw 'Kube nenkinga asikwazanga ukufunda ifayela lakho!'; 
+    }
+}
+// Testing the function
+let path = "inspirationalquotes.txt";
+let quotes = readFileToArray(path);
+
+console.log(quotes);
